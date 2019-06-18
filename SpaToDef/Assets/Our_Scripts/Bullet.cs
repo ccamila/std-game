3cc2a139c0c7c3d8dd53e247e033b707d069db74
camzp <camilacpbc@gmail.com>
2019-06-17 21:37:17 -0300

    Turret Update

--- SpaToDef/Assets/Our_Scripts/Bullet.cs
+++ SpaToDef/Assets/Our_Scripts/Bullet.cs
@@ -5,7 +5,10 @@ public class Bullet : MonoBehaviour{
 
     private Transform target;
     public float speed = 70f;
+    public float explosionField 
     public GameObject impactFX;
+    
+
     public void Chase(Transform t)
     {
         target = t;
@@ -28,12 +31,30 @@ public class Bullet : MonoBehaviour{
             return;
         }
         transform.Translate(dir.normalized * distance, Space.World); //Mover a umavelocidade constante
+        transform.LookAt(target);
     }
 
     void Hit(){
         GameObject fxInst = (GameObject)Instantiate(impactFX, transform.position, transform.rotation);
         Destroy(fxInst, 2f);
+
+        if (explosionField > 0f)
+        {
+            Explode();
+        }
+        else
+        {
+          
+        }
+
         Destroy(target.gameObject);
         PlayerStats.money += 100;
     }
+
+    void Damage (Transform enemy)
+    {
+        Destroy(target.gameObject);
+    }
+
+
 }
---
+++ SpaToDef/Assets/PathCreator/Examples/Scripts/GeneratePathExample.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace PathCreation.Examples
+{
+    // Example of creating a path at runtime from a set of points.
+    // Moves along the generated path.
+
+    [RequireComponent(typeof(TrailRenderer))]
+    public class GeneratePathExample : MonoBehaviour
+    {
+        public Transform[] waypoints;
+        public float speed = 8;
+
+        float dstTravelled;
+        VertexPath path;
+
+        void Start()
+        {
+            if (waypoints.Length > 0)
+            {
+                // Create a new bezier path from the waypoints.
+                // The 'true' argument specifies that the path should be a closed loop
+                BezierPath bezierPath = new BezierPath(waypoints, true, PathSpace.xyz);
+                // Create a vertex path from the bezier path
+                path = new VertexPath(bezierPath);
+            }
+        }
+
+        void Update()
+        {
+            if (path != null)
+            {
+                dstTravelled += speed * Time.deltaTime;
+                transform.position = path.GetPointAtDistance(dstTravelled);
+            }
+        }
+
+    }
+}
